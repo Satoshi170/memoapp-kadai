f46626ce2bcc8e8eb02390d99f5264b0d1f599a1
稲生里史 <inaosatoshi@inaosatoshinonotopasokon.local>
2023-02-08 10:51:23 +0900

    課題を再提出します

--- memo.rb
+++ memo.rb
@@ -3,10 +3,9 @@ require "csv"
 puts "1(新規でメモを作成) 2(既存のメモを編集する)"
 memo_type = gets.to_i
 
-if memo_type == 1 || memo_type == 2
-  puts "新規でメモを作成します。拡張子を除いたファイル名を入力してください。" if memo_type == 1
-  puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください。" if memo_type == 2
-  file_name = gets.to_s
+if memo_type == 1 
+  puts "新規でメモを作成します。拡張子を除いたファイル名を入力してください。" 
+  file_name = gets.chomp
   
   puts "メモしたい内容を記入してください"
   puts "完了したらCtrl + Dをおします"
@@ -17,6 +16,18 @@ if memo_type == 1 || memo_type == 2
     csv.puts ["#{memo}"]
   end
 
+elsif memo_type == 2
+  puts "既存のメモを編集します。拡張子を除いた既存ファイル名を入力してください。"  
+  file_name = gets.chomp
+  
+  puts "メモしたい内容を記入してください"
+  puts "完了したらCtrl + Dをおします"
+  input_memo = STDIN.read
+  memo = input_memo.chomp
+  
+  CSV.open("#{file_name}.csv" , "a") do |csv|
+    csv.puts ["#{memo}"]
+  end
 else
   puts "1か2の数字を入力してください"
 end  
